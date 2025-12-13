c641043276380f477071e46f8d17eb0135af7287
Aniruddharosette <aniruddha@rosettesmartlife.com>
2025-12-13 10:32:46 +0530

    commit

--- server.test.js
+++ server.test.js
@@ -16,6 +16,7 @@ describe('Server Endpoints', () => {
         expect(res.statusCode).toEqual(200);
         
         // 2. Check if the text matches exactly
-        expect(res.text).toContain('Hello from Azure App Service! 11111111');
+        expect(res.text).toContain('Hello from Azure App Service! 22222');
+
     });
 });
